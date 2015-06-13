667ab33f01ccf5bfee89c36eac3b3f51a32087f5
Axel Venet <kern046@gmail.com>
2015-06-13 23:05:56 +0200

    Read the basic json configuration

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
 import(
+	"io/ioutil"
+	"gleipnir/error"
+	"encoding/json"
+	
 )
+
+type Configuration struct {
+
+	Services []string `json: "services"`
+
+}
+
+func main() {
+
+	data, err := ioutil.ReadFile("config.json")
+	error.Check(err)
+	
+	var config Configuration
+	
+	err = json.Unmarshal([]byte(data), &config)
+	error.Check(err)
+}
